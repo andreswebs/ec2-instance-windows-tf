15892f9a70f73b72cc93277efea871b83b330f37
Andre Silva <andreswebs@pm.me>
2022-11-01 16:18:50 -0300

    update windows server 2022; add backend.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_ami" "windows" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["Windows_Server-2019-English-Full-Base-*"]
+    values = ["Windows_Server-2022-English-Full-Base-*"]
   }
   filter {
     name   = "virtualization-type"
